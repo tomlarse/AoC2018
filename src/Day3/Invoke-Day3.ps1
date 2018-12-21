a5e976a9e124d96a6ee6c285daba9b3aba231cdd
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2018-12-21 19:54:14 +0100

    ...

--- src/Day3/Invoke-Day3.ps1
+++ src/Day3/Invoke-Day3.ps1
@@ -11,9 +11,12 @@ function Invoke-Day3 {
         $xadd = $claim.substring($claim.indexof(':')+2,($claim.indexof('x')-$claim.indexof(':')-2))
         $yadd = $claim.substring($claim.indexof('x')+1)
 
-        for ($x = $xadd;$x -gt 0;$x--) {
-            for ($y = $yadd;$y -gt 0;$y--) {
-
+        for ($x = $xadd-1;$x -ge 0;$x--) {
+            for ($y = $yadd-1;$y -ge 0;$y--) {
+                $inchtoclaim = @{
+                    x = $x + $startx
+                    y = $y + $starty
+                }
             }
         }
 
